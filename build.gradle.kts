649fa05ca2ebd2b086adef9749f6eceedc31541d
GUILHERME NUNES <gui.nunes1983@gmail.com>
2026-08-19 18:02:03 -0300

    build: set plugin applications to false in root gradle
    Configure root build script to apply plugins non-transitively to support multi-module project structure.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,6 @@
 plugins {
-  alias(libs.plugins.android.application)
-  alias(libs.plugins.kotlin.android)
-  alias(libs.plugins.kotlin.compose)
-  alias(libs.plugins.google.devtools.ksp)
-  alias(libs.plugins.roborazzi)
-  alias(libs.plugins.secrets)
-  alias(libs.plugins.google.services)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.google.devtools.ksp) apply false
 }
