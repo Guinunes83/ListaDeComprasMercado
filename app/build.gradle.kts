e8680073efa66afd0d13f1b45c813b208ad64ebd
GUILHERME NUNES <gui.nunes1983@gmail.com>
2026-08-20 14:45:30 -0300

    Modify plugin application settings in build.gradle.kts
    Updated plugin application settings in build.gradle.kts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,13 @@
 import com.google.gms.googleservices.GoogleServicesPlugin.MissingGoogleServicesStrategy
 
 plugins {
-  alias(libs.plugins.android.application)
-  alias(libs.plugins.kotlin.android)
-  alias(libs.plugins.google.devtools.ksp)
-  alias(libs.plugins.roborazzi)
-  alias(libs.plugins.secrets)
-  alias(libs.plugins.google.services)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false // <- GARANTIR ESTA LINHA
+    alias(libs.plugins.google.devtools.ksp) apply false
 }
 
+  
 android {
   namespace = "com.example"
   compileSdk = 35
